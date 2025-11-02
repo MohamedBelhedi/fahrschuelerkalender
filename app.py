eb0d256faaa3c3883785308724ed5bf3ae319e38
Mohamed <mbelhedi@outlook.com>
2025-11-02 12:49:01 +0100

    Branch 0.0.1 Kalendar ready req.txt

--- app.py
+++ app.py
@@ -123,4 +123,4 @@ def callAdmin():
     return render_template("admin.html", appointments=appointments, lang=lang)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080 ,debug=True)
+    app.run(host="0.0.0.0", port=8080 ,debug=False)
